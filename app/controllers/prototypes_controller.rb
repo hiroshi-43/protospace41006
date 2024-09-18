1ffb4f53facb1eb21d4786524fc5b504e265e73c
hiroshi-43 <piropirorock@gmail.com>
2024-09-18 14:56:29 +0900

    完成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,60 @@
 class PrototypesController < ApplicationController
+  
+before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def index
+    @prototypes = Prototype.all
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+        redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end    
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  private
+
+  def ensure_correct_user
+    prototype = Prototype.find(params[:id])
+    unless prototype.user == current_user
+      redirect_to root_path
+    end
+  end  
+
 end
